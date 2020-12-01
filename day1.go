b2fe6cf4d9a36c870d4577b8e4df48b639cebe28
Vincent Verhoeven <verhoevenv@gmail.com>
2020-12-02 00:11:30 +0100

    Day 1 part 2

--- day1.go
+++ day1.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 )
 
-func ElemsThatSumTo2020(report []int) (a, b int) {
+func PairThatSumsTo2020(report []int) (a, b int) {
 	for i1, v1 := range report {
 		for _, v2 := range report[i1:] {
 			if v1+v2 == 2020 {
@@ -18,9 +18,22 @@ func ElemsThatSumTo2020(report []int) (a, b int) {
 	panic("No summing pair found")
 }
 
+func TripletThatSumsTo2020(report []int) (a, b, c int) {
+	for i1, v1 := range report {
+		for i2, v2 := range report[i1:] {
+			for _, v3 := range report[i2:] {
+				if v1+v2+v3 == 2020 {
+					return v1, v2, v3
+				}
+			}
+		}
+	}
+	panic("No summing triplet found")
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	numbers := make([]int, 5)
+	numbers := make([]int, 0)
 	for scanner.Scan() {
 		i, err := strconv.Atoi(scanner.Text())
 		if err != nil {
@@ -29,7 +42,7 @@ func main() {
 		numbers = append(numbers, i)
 	}
 
-	first, second := ElemsThatSumTo2020(numbers)
-	result := first * second
+	first, second, third := TripletThatSumsTo2020(numbers)
+	result := first * second * third
 	fmt.Println(result)
 }
--- day1_test.go
+++ day1_test.go
@@ -2,10 +2,18 @@ package main
 
 import "testing"
 
-func Test(t *testing.T) {
-	first, second := ElemsThatSumTo2020([]int{1721, 979, 366, 299, 675, 1456})
+func TestPair(t *testing.T) {
+	first, second := PairThatSumsTo2020([]int{1721, 979, 366, 299, 675, 1456})
 	if first != 1721 && second != 299 {
 		t.Errorf("Elems was incorrect, got: %d and %d, want: %d and %d.",
 			first, second, 1721, 299)
+	}
+}
+
+func TestTriplet(t *testing.T) {
+	first, second, third := TripletThatSumsTo2020([]int{1721, 979, 366, 299, 675, 1456})
+	if first != 979 && second != 366 && third != 675 {
+		t.Errorf("Elems was incorrect, got: %d, %d and %d, want: %d, %d and %d.",
+			first, second, third, 979, 366, 675)
 	}
 }
