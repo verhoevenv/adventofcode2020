3daf890eefa010f21f13e4389c905c6169ae84b2
Vincent Verhoeven <verhoevenv@gmail.com>
2020-12-11 23:18:13 +0100

    Day 11 part 2

--- day11/day11.go
+++ day11/day11.go
@@ -37,12 +37,37 @@ func makeLayout(layoutStr string) *layout {
 	return &layout{result, xy{maxX, maxY}}
 }
 
-var neighboursCache = make(map[xy][]xy)
+type rules struct {
+	neighbourRule neighbouringSeats
+	tolerance     int
+}
 
-func (l *layout) neighbours(loc *xy) []xy {
-	if v, ok := neighboursCache[*loc]; ok {
-		return v
+var part1 = rules{
+	withCache(surroundingNeighbours),
+	4,
+}
+
+var part2 = rules{
+	withCache(visibleNeighbours),
+	5,
+}
+
+type neighbouringSeats func(l *layout, loc *xy) []xy
+
+func withCache(f neighbouringSeats) neighbouringSeats {
+	var neighboursCache = make(map[xy][]xy)
+
+	return func(l *layout, loc *xy) []xy {
+		if v, ok := neighboursCache[*loc]; ok {
+			return v
+		}
+		result := f(l, loc)
+		neighboursCache[*loc] = result
+		return result
 	}
+}
+
+func surroundingNeighbours(l *layout, loc *xy) []xy {
 	result := make([]xy, 0)
 	for _, dx := range []int{-1, 0, 1} {
 		for _, dy := range []int{-1, 0, 1} {
@@ -54,7 +79,31 @@ func (l *layout) neighbours(loc *xy) []xy {
 			}
 		}
 	}
-	neighboursCache[*loc] = result
+	return result
+}
+
+func visibleNeighbours(l *layout, loc *xy) []xy {
+	result := make([]xy, 0)
+	for _, dx := range []int{-1, 0, 1} {
+		for _, dy := range []int{-1, 0, 1} {
+			if !(dx == 0 && dy == 0) {
+				dist := 0
+				stop := false
+				for !stop {
+					dist++
+					candidate := xy{loc.x + dx*dist, loc.y + dy*dist}
+					if candidate.x < 0 || candidate.x >= l.size.x ||
+						candidate.y < 0 || candidate.y >= l.size.y {
+						stop = true
+					} else if l.grid[candidate] == '#' ||
+						l.grid[candidate] == 'L' {
+						result = append(result, candidate)
+						stop = true
+					}
+				}
+			}
+		}
+	}
 	return result
 }
 
@@ -78,14 +127,14 @@ func (l *layout) countAll(s state) int {
 	return counter
 }
 
-func (l *layout) round() {
+func (l *layout) round(rules rules) {
 	result := make(map[xy]state)
 
 	for loc, s := range l.grid {
 		switch {
-		case s == 'L' && l.count('#', l.neighbours(&loc)) == 0:
+		case s == 'L' && l.count('#', rules.neighbourRule(l, &loc)) == 0:
 			result[loc] = '#'
-		case s == '#' && l.count('#', l.neighbours(&loc)) >= 4:
+		case s == '#' && l.count('#', rules.neighbourRule(l, &loc)) >= rules.tolerance:
 			result[loc] = 'L'
 		default:
 			result[loc] = s
@@ -95,10 +144,10 @@ func (l *layout) round() {
 	l.grid = result
 }
 
-func (l *layout) untilStable() {
+func (l *layout) untilStable(rules rules) {
 	for {
 		old := l.grid
-		l.round()
+		l.round(rules)
 		if reflect.DeepEqual(old, l.grid) {
 			break
 		}
@@ -109,7 +158,7 @@ func main() {
 	input, _ := ioutil.ReadAll(os.Stdin)
 
 	layout := makeLayout(string(input))
-	layout.untilStable()
+	layout.untilStable(part2)
 
 	fmt.Println(layout.countAll('#'))
 }
--- day11/day11_test.go
+++ day11/day11_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-var seatings = []string{
+var seatingsPart1 = []string{
 	`L.LL.LL.LL
 LLLLLLL.LL
 L.L.L..L..
@@ -74,12 +74,12 @@ L.#.L..#..
 }
 
 func TestRound(t *testing.T) {
-	layout := makeLayout(seatings[0])
+	layout := makeLayout(seatingsPart1[0])
 
-	for i := 1; i < len(seatings); i++ {
-		layout.round()
+	for i := 1; i < len(seatingsPart1); i++ {
+		layout.round(part1)
 
-		expected := makeLayout(seatings[i])
+		expected := makeLayout(seatingsPart1[i])
 
 		if !reflect.DeepEqual(layout, expected) {
 			t.Errorf("Round from %v to %v was incorrect, got: %v, want: %v.",
@@ -89,11 +89,131 @@ func TestRound(t *testing.T) {
 }
 
 func TestUntilStable(t *testing.T) {
-	layout := makeLayout(seatings[0])
+	layout := makeLayout(seatingsPart1[0])
 
-	layout.untilStable()
+	layout.untilStable(part1)
 
-	expected := makeLayout(seatings[len(seatings)-1])
+	expected := makeLayout(seatingsPart1[len(seatingsPart1)-1])
+
+	if !reflect.DeepEqual(layout, expected) {
+		t.Errorf("UntilStable was incorrect, got: %v, want: %v.",
+			layout, expected)
+	}
+
+}
+
+var seatingsPart2 = []string{
+	`L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL`,
+
+	`#.##.##.##
+#######.##
+#.#.#..#..
+####.##.##
+#.##.##.##
+#.#####.##
+..#.#.....
+##########
+#.######.#
+#.#####.##`,
+
+	`#.LL.LL.L#
+#LLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLLL.L
+#.LLLLL.L#`,
+
+	`#.L#.##.L#
+#L#####.LL
+L.#.#..#..
+##L#.##.##
+#.##.#L.##
+#.#####.#L
+..#.#.....
+LLL####LL#
+#.L#####.L
+#.L####.L#`,
+
+	`#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##LL.LL.L#
+L.LL.LL.L#
+#.LLLLL.LL
+..L.L.....
+LLLLLLLLL#
+#.LLLLL#.L
+#.L#LL#.L#`,
+
+	`#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##L#.#L.L#
+L.L#.#L.L#
+#.L####.LL
+..#.#.....
+LLL###LLL#
+#.LLLLL#.L
+#.L#LL#.L#`,
+
+	`#.L#.L#.L#
+#LLLLLL.LL
+L.L.L..#..
+##L#.#L.L#
+L.L#.LL.L#
+#.LLLL#.LL
+..#.L.....
+LLL###LLL#
+#.LLLLL#.L
+#.L#LL#.L#`,
+}
+
+func TestVisibleNeighbours(t *testing.T) {
+	layout := makeLayout(seatingsPart2[0])
+
+	result := visibleNeighbours(layout, &xy{0, 0})
+
+	expected := []xy{xy{0, 1}, xy{2, 0}, xy{1, 1}}
+	if !reflect.DeepEqual(result, expected) {
+		t.Errorf("visibleNeighbours incorrect, got: %v, want: %v.",
+			result, expected)
+	}
+}
+
+func TestRound2(t *testing.T) {
+	layout := makeLayout(seatingsPart2[0])
+
+	for i := 1; i < len(seatingsPart2); i++ {
+		layout.round(part2)
+
+		expected := makeLayout(seatingsPart2[i])
+
+		if !reflect.DeepEqual(layout, expected) {
+			t.Errorf("Round from %v to %v was incorrect, got: %v, want: %v.",
+				i-1, i, layout, expected)
+		}
+	}
+}
+
+func TestUntilStable2(t *testing.T) {
+	layout := makeLayout(seatingsPart2[0])
+
+	layout.untilStable(part2)
+
+	expected := makeLayout(seatingsPart2[len(seatingsPart2)-1])
 
 	if !reflect.DeepEqual(layout, expected) {
 		t.Errorf("UntilStable was incorrect, got: %v, want: %v.",
